ceede0ab9acfb405cf277741a678972cf6713762
paullaster <paullasterokoth98@gmail.com>
2023-02-01 16:29:30 +0300

    commit fontend start up files

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,3 @@
   </div>
 </template>
 
-<script>
- import logo from "../public/mata/PNG(transparent background)/meta ajax-02.png";
-
- export default {
-  data(){
-    return {
-      logo: logo
-    }
-  }
- }
-</script>
---
+++ src/components/LogoComponent.vue
@@ -0,0 +1,12 @@
+
+<script>
+ import logo from "../public/mata/PNG(transparent background)/meta ajax-02.png";
+
+ export default {
+  data(){
+    return {
+      logo: logo
+    }
+  }
+ }
+</script>
